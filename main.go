4ac3062cf58db8852296c1bac2826ea799fee1cd
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2024-07-27 23:05:29 +0500

    + Update rotatinglogger init

--- main.go
+++ main.go
@@ -3,25 +3,47 @@ package main
 import (
 	"github.com/sirupsen/logrus"
 	"rolog-example/internal/rotatinglogger"
+	"strconv"
 	"time"
 )
 
 func main() {
-	// Настройки логирования
+	// Logger Settings
 	logDir := "logs"
 	staticFilename := "application.log"
 	archivePattern := "application-%s.log"
-	zippedArchive := true
-	maxSize := 20              // 20 MB
-	maxAge := 14               // 14 days
-	maxBackups := 3            // 3 backups
-	checkInterval := time.Hour // Check every hour
-	bufferSize := 100          // Size of the log message buffer
+	zippedArchive := true        // Enable zipped archive
+	maxSize := 1                 // 20 MB
+	maxBackups := 3              // 3 backups
+	checkInterval := time.Second // Check every hour
+	bufferSize := 100            // Size of the log message buffer
+	logLevel := logrus.InfoLevel // Set the log level to Info
 
-	logger := rotatinglogger.NewRotatingLogger(logDir, staticFilename, archivePattern, zippedArchive, maxSize, maxAge, maxBackups, checkInterval, bufferSize)
+	//logger := rotatinglogger.NewRotatingLogger(logDir, staticFilename, archivePattern, zippedArchive, maxSize, maxBackups, checkInterval, bufferSize, logLevel)
+
+	logger := rotatinglogger.NewRotatingLogger(
+		logDir,
+		staticFilename,
+		archivePattern,
+		zippedArchive,
+		maxSize,
+		maxBackups,
+		checkInterval,
+		bufferSize,
+		logLevel,
+	)
 
 	for {
-		logger.Log(logrus.InfoLevel, "This is a log message.", logrus.Fields{"appName": "exampleApp"})
-		time.Sleep(1 * time.Hour)
+		logger.Log(logrus.InfoLevel, "This is an info log message.", logrus.Fields{"appName": "exampleApp"})
+		logger.Log(logrus.DebugLevel, "This is a debug log message.", logrus.Fields{"appName": "exampleApp"})
+
+		// Generate some log messages - 1000 messages
+		for i := 0; i < 1000; i++ {
+			// i to string
+			s := strconv.Itoa(i)
+			logger.Log(logrus.InfoLevel, "This is an info log message. Number: "+s, logrus.Fields{"appName": "exampleApp"})
+		}
+
+		time.Sleep(1 * time.Second)
 	}
 }
