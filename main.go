a18bdadaec0a47eae3eeb840d653a1e069ee7e61
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2024-07-28 17:32:40 +0500

    + Tume tester

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ func GenerateLogs() {
 
 		// Generate some log messages - 1000 messages
 		for i := 0; i < 1000; i++ {
-			// i to string
 			s := strconv.Itoa(i)
 			ro.Logger.Log(logrus.InfoLevel, "This is an info log message. With item: "+s, logrus.Fields{"appName": "exampleApp"})
 		}
@@ -26,23 +25,50 @@ func GenerateLogs() {
 // GenerateChannelLogs - Generate logs using channel
 func GenerateChannelLogs(id int, data chan int) {
 	for taskId := range data {
+		sId := strconv.Itoa(id)
+		ro.Logger.Log(logrus.InfoLevel, "This is an info log message from channel. With worker and id: "+sId+"-"+strconv.Itoa(taskId), logrus.Fields{"appName": "exampleApp"})
+
 		time.Sleep(2 * time.Second)
-		ro.Logger.Log(logrus.InfoLevel, "This is an info log message from channel. With item: "+strconv.Itoa(taskId), logrus.Fields{"appName": "exampleApp"})
 	}
 }
 
+// RunTimeTest - Run time test
+func RunTimeTest() {
+	var totalDuration time.Duration
+	runs := 100
+	messagesPerRun := 1000
+
+	for i := 0; i < runs; i++ {
+		start := time.Now()
+
+		for j := 0; j < messagesPerRun; j++ {
+			ro.Logger.Log(logrus.InfoLevel, "This is an info log message.", logrus.Fields{"appName": "MyApp"})
+		}
+
+		duration := time.Since(start)
+		totalDuration += duration
+		logrus.Infof("Run %d: Time taken to log %d messages: %v", i+1, messagesPerRun, duration)
+	}
+
+	averageDuration := totalDuration / time.Duration(runs)
+	logrus.Infof("Average time taken to log %d messages over %d runs: %v", messagesPerRun, runs, averageDuration)
+
+}
+
 func main() {
+
+	RunTimeTest()
 
 	// Create channel
 	channel := make(chan int)
 
 	// Creating 100 workers to execute task
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 1; i++ {
 		go GenerateChannelLogs(i, channel)
 	}
 
-	// Filling channel with data
-	for i := 0; i < 100000; i++ {
+	// Filling channel with 100000 data
+	for i := 0; i < 10; i++ {
 		channel <- i
 	}
 
