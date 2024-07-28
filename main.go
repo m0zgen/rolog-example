6e38f536e1cc16bd5822e2a7d66a13764377759e
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2024-07-28 17:11:02 +0500

    + Generate Logs with multiple channel

--- main.go
+++ main.go
@@ -23,8 +23,30 @@ func GenerateLogs() {
 
 }
 
+// GenerateChannelLogs - Generate logs using channel
+func GenerateChannelLogs(id int, data chan int) {
+	for taskId := range data {
+		time.Sleep(2 * time.Second)
+		ro.Logger.Log(logrus.InfoLevel, "This is an info log message from channel. With item: "+strconv.Itoa(taskId), logrus.Fields{"appName": "exampleApp"})
+	}
+}
+
 func main() {
 
+	// Create channel
+	channel := make(chan int)
+
+	// Creating 100 workers to execute task
+	for i := 0; i < 100; i++ {
+		go GenerateChannelLogs(i, channel)
+	}
+
+	// Filling channel with data
+	for i := 0; i < 100000; i++ {
+		channel <- i
+	}
+
+	// Simple log generation
 	GenerateLogs()
 
 }
